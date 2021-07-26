f181a934903d93772b3d32860e01f4dd56f2345e
kobimodel5 <sromasterw@gmail.com>
2021-07-26 22:03:13 +0300

    setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeeNTs3quWhZel3lUgWCcoV5gHGE39bc4",
+  authDomain: "eshop-baigiamsi.firebaseapp.com",
+  projectId: "eshop-baigiamsi",
+  storageBucket: "eshop-baigiamsi.appspot.com",
+  messagingSenderId: "461562580154",
+  appId: "1:461562580154:web:c23ded2d859b318e2c0683",
+};
